f4f98bae984bfc256a8a66178a20caaa7b59c8f6
ShaiMaytav <shaimaytav01@gmail.com>
2021-12-07 17:02:15 +0200

    Added 2 Script files
    Added PlayerController script file
    Added EnemyAI script file

---
+++ FUTWindow/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject player;
+    public Rigidbody2D rb;
+    public float movementSpeed;
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    public void Punch()
+    {
+        
+    }
+}
