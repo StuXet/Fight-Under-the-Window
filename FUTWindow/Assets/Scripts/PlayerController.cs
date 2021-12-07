03d7e7f95dc6bfa1efe165b5e56f2b98da5113e8
ShaiMaytav <shaimaytav01@gmail.com>
2021-12-07 19:14:37 +0200

    Created Working Controller
    -Wrote a simple movement controller for the player
    -Added bounds to the scene
    
    Notes:
    optimize movement with animations

--- FUTWindow/Assets/Scripts/PlayerController.cs
+++ FUTWindow/Assets/Scripts/PlayerController.cs
@@ -5,17 +5,48 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public GameObject player;
+    public Animator anim;
     public Rigidbody2D rb;
-    public float movementSpeed;
+    public FixedJoystick joystick;
+    public float movementSpeed = 10;
+    private float hor;
+    private float ver;
+    private string dir;
 
-    //// Update is called once per frame
-    //void Update()
-    //{
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+        //Animate();
+    }
 
-    //}
+    private void FixedUpdate()
+    {
+        Move();
+        
+    }
 
-    public void Punch()
+    public void CheckInput()
+    {
+        hor = joystick.Horizontal;
+        ver = joystick.Vertical;
+    }
+
+    public void Move()
+    {
+        if (Mathf.Abs(hor) >= 0.5f || Mathf.Abs(ver) >= 0.5f)
+        {
+            rb.velocity = new Vector2(hor * movementSpeed, ver * movementSpeed);
+        }
+        else if (Mathf.Abs(hor) == 0 && Mathf.Abs(ver) == 0)
+        {
+            rb.velocity = new Vector2(hor * movementSpeed, ver * movementSpeed);
+        }
+    }
+
+    public void Animate()
     {
         
     }
+   
 }
