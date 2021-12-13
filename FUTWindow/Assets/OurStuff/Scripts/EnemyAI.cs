df814c33387c75bd975f8a7ae254609945b54c1b
RoxasCodes <evicokazzz@gmail.com>
2021-12-13 20:22:22 +0200

    enemy ai.
    not finished, no animation

--- FUTWindow/Assets/OurStuff/Scripts/EnemyAI.cs
+++ FUTWindow/Assets/OurStuff/Scripts/EnemyAI.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    public float chaseDistance;
+    public float stopDistance;
+    public GameObject target;
+
+    private float targetDistance;
+    Animator animator;
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        targetDistance = Vector2.Distance(transform.position, target.transform.position);
+        if (targetDistance < chaseDistance && targetDistance > stopDistance)
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            StopChasePlayer();
+        }
+    }
+
+    private void StopChasePlayer()
+    {
+        animator.SetBool("IsWalking", false);
+    }
+
+    private void ChasePlayer()
+    {
+        if (transform.position.x < target.transform.position.x)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+        animator.SetBool("IsWalking", true);
     }
 }
