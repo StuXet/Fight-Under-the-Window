b32df5a882b3342d37c428090912ab1c9cf20db4
ShaiMaytav <shaimaytav01@gmail.com>
2022-01-18 20:07:26 +0200

    yikfhv

--- FUTWindow/Assets/OurStuff/Scripts/WaveSpawner.cs
+++ FUTWindow/Assets/OurStuff/Scripts/WaveSpawner.cs
@@ -39,7 +39,7 @@ public class WaveSpawner : MonoBehaviour
 
     void Start()
     {
-
+        SoundManagerScript.PlaySound("SRV");
         timeBetweenWaves = waveCountdown;
         PauseGame();
     }
